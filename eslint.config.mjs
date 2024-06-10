fc6f737787d63a8c21b48a1d7afc7b793e33bd5b
Guilherme Araújo <guilherme.araujo@maxxidata.com>
2024-06-10 13:01:57 -0300

    Set up ESLint and Prettier (#12)
    * Set up Prettier
    
    * Set up ESLint
    
    * Create .editorconfig
    
    * Create pre-commit hook
    
    * chore: add eslint rules link

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  // Visit https://eslint.org/docs/latest/rules to learn more about these rules
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+];
