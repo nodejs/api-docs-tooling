a009ec48fcc4ee51b8f933303d048fd3c0edf103
Claudio Wunder <cwunder@gnome.org>
2024-07-08 04:29:59 +0100

    chore: deep optimization and fix of edge cases

--- src/parser.mjs
+++ src/parser.mjs
@@ -5,9 +5,10 @@ import { VFile } from 'vfile';
 import { remark } from 'remark';
 import remarkGfm from 'remark-gfm';
 
-import { SKIP, visit } from 'unist-util-visit';
+import { u } from 'unist-builder';
 import { remove } from 'unist-util-remove';
 import { selectAll } from 'unist-util-select';
+import { SKIP, visit } from 'unist-util-visit';
 import { findAfter } from 'unist-util-find-after';
 import { findAllAfter } from 'unist-util-find-all-after';
 
@@ -15,10 +16,7 @@ import createMetadata from './metadata.mjs';
 import createQueries from './queries.mjs';
 
 import { createNodeSlugger } from './utils/slugger.mjs';
-import { callIfBefore } from './utils/parser.mjs';
-
-// Characters used to split each section within an API Doc file
-const DOC_API_ENTRY_SEPARATOR = /^#{1,4} .*/gm;
+import { callIfBefore } from './utils/unist.mjs';
 
 /**
  * Creates an API doc parser for a given Markdown API doc file
@@ -47,7 +45,7 @@ const createParser = () => {
      * Then once we have the whole file parsed, we can split the resulting string into sections
      * and seal the Metadata Entries (`.create()`) and return the result to the caller of parae.
      *
-     * @type {Array<ReturnType<import('./metadata.mjs').default>}
+     * @type {Array<import('./types.d.ts').ApiDocMetadataEntry>}
      */
     const metadataCollection = [];
 
@@ -59,6 +57,11 @@ const createParser = () => {
 
     // Adds a transformer for handling link references and Markdown definitions
     apiDocProcessor.use(() => {
+      /**
+       * This transformer is responsible for handling link references and Markdown definitions
+       *
+       * @param {import('unist').Parent} tree The root AST tree for the API doc file
+       */
       return tree => {
         // Get all Markdown Footnote definitions from the tree
         const markdownDefinitions = selectAll('definition', tree);
@@ -79,6 +82,11 @@ const createParser = () => {
     // Adds a transformer for normalising the Markdown file with numerous actions
     // that are required for the API docs to be correctly parsed
     apiDocProcessor.use(() => {
+      /**
+       * This transformer is responsible for normalising the Markdown file
+       *
+       * @param {import('unist').Parent} tree The root AST tree for the API doc file
+       */
       return tree => {
         // Handles API type references transformation into links
         visit(tree, createQueries.UNIST.isTextWithType, node => {
@@ -100,8 +108,13 @@ const createParser = () => {
     // and going over the sections and creating metadata entries by traversing the tree
     // A new metadata entry is created once a new Heading is found (from level 1 to 6)
     apiDocProcessor.use(() => {
+      /**
+       * Iterates through the AST tree and creates Metadata entries for each API doc section
+       *
+       * @param {import('unist').Parent} tree The root AST tree for the API doc file
+       */
       return tree => {
-        visit(tree, createQueries.UNIST.isHeadingNode, node => {
+        visit(tree, createQueries.UNIST.isHeadingNode, (node, index) => {
           // Creates a new Metadata entry for the current API doc file
           const apiEntryMetadata = createMetadata(nodeSlugger);
 
@@ -111,7 +124,10 @@ const createParser = () => {
           // We retrieve the immediate next Heading if it exists
           // This is used for ensuring that we don't include items that would
           // belong only to the next heading to the current Heading metadata
-          callIfBefore(findAfter(tree, node, 'heading'), node, next => {
+          // Note that if there is no next heading, we use the current node as the next one
+          const nextHeadingNode = findAfter(tree, index, 'heading') ?? node;
+
+          callIfBefore(nextHeadingNode, node, next => {
             // Gets the next available Stability Index Node (if any)
             // from the current Heading Node, and then adds it to the current Metadata
             // if the found Stability Index Node is before the next Heading Node
@@ -133,7 +149,11 @@ const createParser = () => {
             );
 
             stabilityIndexNodes.forEach(stabilityIndexNode => {
-              callIfBefore(next, stabilityIndexNode, () => {
+              // If the next heading === current one, then we use the stabilityIndex
+              // itself as the check, since this means we reaching the end of the document
+              const toCheckAgainst = node === next ? stabilityIndexNode : next;
+
+              callIfBefore(toCheckAgainst, stabilityIndexNode, () => {
                 // Adds the Stability Index Metadata to the current Metadata entry
                 addStabilityIndexMetadata(stabilityIndexNode, apiEntryMetadata);
 
@@ -142,45 +162,69 @@ const createParser = () => {
             });
 
             yamlMetadataNodes.forEach(yamlMetadataNode => {
-              callIfBefore(next, yamlMetadataNode, () => {
+              // If the next heading === current one, then we use the yamlMetadataNode
+              // itself as the check, since this means we reaching the end of the document
+              const toCheckAgainst = node === next ? yamlMetadataNode : next;
+
+              callIfBefore(toCheckAgainst, yamlMetadataNode, () => {
                 // Adds the YAML Metadata to the current Metadata entry
                 addYAMLMetadata(yamlMetadataNode, apiEntryMetadata);
 
                 remove(tree, yamlMetadataNode);
               });
             });
-          });
 
-          // After all is processed for the current Heading we proceed to push it
-          // to the Metadata collection, so that it can be sealed later on
-          metadataCollection.push(apiEntryMetadata);
+            // This is the cutover index of the subtree that we should get
+            // of all the Nodes within the AST tree that belong to this section
+            // If `next` is equals the current heading, it means ther's no next heading
+            // and we are reaching the end of the document, hence the cutover should be the end of
+            // the document itself,
+            // Note.: This index needs to be retrieved after any modification to the tree occurs,
+            // otherwise the index will be off (out of sync with the tree)
+            const cutoverIndex =
+              node === next
+                ? tree.children.length - 1
+                : tree.children.indexOf(next);
+
+            // Retrieves all the Nodes that should belong to the current API doc section
+            const sectionNodes = tree.children.slice(index + 1, cutoverIndex);
+
+            // The stringified (back to Markdown) content of the section
+            const sectionParsedContent = apiDocProcessor.stringify(
+              // Creates a subtree based on the section Nodes
+              u('root', sectionNodes),
+              apiDoc
+            );
+
+            // We seal and create the API doc entry Metadata and push them to the collection
+            // Creates the API doc entry Metadata and pushes it to the collection
+            const parsedApiEntryMetadata = apiEntryMetadata.create(
+              apiDoc.stem,
+              // Creates a VFile for the current section content
+              new VFile({ ...apiDoc, value: sectionParsedContent })
+            );
+
+            // We push the parsed API doc entry Metadata to the collection
+            metadataCollection.push(parsedApiEntryMetadata);
+
+            // Finally, we remove the Heading from the tree as it's no longer needed
+            remove(tree, node);
+          });
 
           return SKIP;
         });
       };
     });
 
-    // Processes the API doc file and returns the parsed API doc
-    const parsedApiDoc = await apiDocProcessor.process(apiDoc);
+    console.log(`[parser] parsing: ${apiDoc.stem}`);
 
-    /**
-     * Splits the parsed API doc file into sections (a section is defined by when a heading is found)
-     * We consider up to level 4 headings as sections, and we split the file into sections
-     *
-     * @type {Array<string>}
-     */
-    const [, ...sections] = String(parsedApiDoc).split(DOC_API_ENTRY_SEPARATOR);
+    // Parses the API doc into an AST tree using `unified` and `remark`
+    const apiDocTree = apiDocProcessor.parse(apiDoc);
 
-    // We iterate the Markdown sections and retrieve the parsed Metadata entries from when Unified
-    // was iterating through the tree and then seal (create) the metadata entry for each section
-    return sections.map((section, index) => {
-      // This creates a new VFile with the top-level metadata of the API doc file
-      // and the trimmed content of the section (which excludes the header, since it is already within the Metadata)
-      const apiEntryFile = new VFile({ ...apiDoc, value: section.trim() });
+    // Applies the AST transformers defined before to the API doc tree
+    await apiDocProcessor.run(apiDocTree);
 
-      // Seals the Metadata entry with the actual content of the section
-      return metadataCollection[index].create(apiDoc.stem, apiEntryFile);
-    });
+    return metadataCollection;
   };
 
   /**
@@ -189,8 +233,11 @@ const createParser = () => {
    *
    * @param {Array<import('vfile').VFile>} apiDocs List of API doc files to be parsed
    */
-  const parseApiDocs = apiDocs =>
-    Promise.all(apiDocs.map(parseApiDoc)).then(entries => entries.flat());
+  const parseApiDocs = async apiDocs => {
+    const apiDocsParsedPromise = await Promise.all(apiDocs.map(parseApiDoc));
+
+    return apiDocsParsedPromise.flat();
+  };
 
   return { parseApiDocs };
 };
--- src/utils/unist.mjs
+++ src/utils/unist.mjs
@@ -1,10 +1,12 @@
 'use strict';
 
+import { pointEnd, pointStart } from 'unist-util-position';
+
 /**
  * This utility allows us to join children Nodes into one
  * and transfor them back to what their source would look like
  *
- * @param {import('unist').Node} nodes Nodes to parsed and joined
+ * @param {Array<import('unist').Parent & import('unist').Literal>} nodes Nodes to parsed and joined
  * @returns {string} The parsed and joined nodes as a string
  */
 export const transformNodesToString = nodes => {
@@ -29,4 +31,23 @@ export const transformNodesToString = nodes => {
   });
 
   return mappedChildren.join('');
+};
+
+/**
+ * This method is an utility that allows us to conditionally invoke/call a callback
+ * based on test conditions related to a Node's position relative to another one
+ * being before or not the other Node
+ *
+ * @param {import('unist').Node | undefined} nodeA The Node to be used as a position reference to check against
+ *  the other Node. If the other Node is before this one, the callback will be called.
+ * @param {import('unist').Node | undefined} nodeB The Node to be checked against the position of the first Node
+ * @param {(nodeA: import('unist').Node, nodeB: import('unist').Node) => void} callback The callback to be called
+ */
+export const callIfBefore = (nodeA, nodeB, callback) => {
+  const positionA = pointEnd(nodeA);
+  const positionB = pointStart(nodeB);
+
+  if (positionA && positionB && positionA.line >= positionB.line) {
+    callback(nodeA, nodeB);
+  }
 };
