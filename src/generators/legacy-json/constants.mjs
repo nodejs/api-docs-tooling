8e500473900c44cba36623f2e85f1e1a472634f1
flakey5 <73616808+flakey5@users.noreply.github.com>
2024-09-26 11:25:33 -0700

    change comment style
    Signed-off-by: flakey5 <73616808+flakey5@users.noreply.github.com>

--- src/generators/legacy-json/constants.mjs
+++ src/generators/legacy-json/constants.mjs
@@ -1,30 +1,18 @@
-/**
- * Denotes a method's return value
- */
+// Grabs a method's return value
 export const RETURN_EXPRESSION = /^returns?\s*:?\s*/i;
 
-/**
- * Denotes a method's name
- */
+// Grabs a method's name
 export const NAME_EXPRESSION = /^['`"]?([^'`": {]+)['`"]?\s*:?\s*/;
 
-/**
- * Denotes a method's type
- */
+// Denotes a method's type
 export const TYPE_EXPRESSION = /^\{([^}]+)\}\s*/;
 
-/**
- * Is there a leading hyphen
- */
+// Checks if there's a leading hyphen
 export const LEADING_HYPHEN = /^-\s*/;
 
-/**
- * Denotes a default value
- */
+// Grabs the default value if present
 export const DEFAULT_EXPRESSION = /\s*\*\*Default:\*\*\s*([^]+)$/i;
 
-/**
- * Grabs the parameters from a method's signature
- * @example 'new buffer.Blob([sources[, options]])'.match(PARAM_EXPRESSION) = ['([sources[, options]])', '[sources[, options]]']
- */
+// Grabs the parameters from a method's signature
+//  ex/ 'new buffer.Blob([sources[, options]])'.match(PARAM_EXPRESSION) === ['([sources[, options]])', '[sources[, options]]']
 export const PARAM_EXPRESSION = /\((.+)\);?$/;
