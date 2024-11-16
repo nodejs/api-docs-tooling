24aaa111c4e851f62d7865654132327ee24276fb
Aviv Keller <redyetidev@gmail.com>
2024-11-16 09:49:49 -0500

    feat: allow non-cli execution (#137)

---
+++ src/index.mjs
@@ -0,0 +1,8 @@
+export * as constants from './constants.mjs';
+export { default as generators } from './generators/index.mjs';
+export { default as createGenerator } from './generators.mjs';
+export { default as createLoader } from './loader.mjs';
+export { default as createMetadata } from './metadata.mjs';
+export { default as createParser } from './parser.mjs';
+export { default as createQueries } from './queries.mjs';
+export { default as createNodeReleases } from './releases.mjs';
