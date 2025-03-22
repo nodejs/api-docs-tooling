17033dfb452a3a08940f738396e7afacecb5f241
avivkeller <me@aviv.sh>
2025-03-22 14:49:41 -0400

    fix(test): work on both CRLF and LF machines

--- src/linter/tests/reporters/github.test.mjs
+++ src/linter/tests/reporters/github.test.mjs
@@ -13,14 +13,14 @@ describe('github', () => {
 
     assert.strictEqual(process.stdout.write.mock.callCount(), 3);
 
-    const callsArgs = process.stdout.write.mock.calls.map(
-      call => call.arguments[0]
+    const callsArgs = process.stdout.write.mock.calls.map(call =>
+      call.arguments[0].trim()
     );
 
     assert.deepStrictEqual(callsArgs, [
-      '::notice file=doc/api/test.md::This is a INFO issue\n',
-      '::warning file=doc/api/test.md,line=1,endLine=1::This is a WARN issue\n',
-      '::error file=doc/api/test.md,line=1,endLine=1::This is a ERROR issue\n',
+      '::notice file=doc/api/test.md::This is a INFO issue',
+      '::warning file=doc/api/test.md,line=1,endLine=1::This is a WARN issue',
+      '::error file=doc/api/test.md,line=1,endLine=1::This is a ERROR issue',
     ]);
   });
 });
