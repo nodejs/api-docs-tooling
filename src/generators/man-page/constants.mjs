194a35751df860b15fb0b2d7afed8cf7bad9b3a8
Guilherme Araújo <arauujogui@gmail.com>
2025-03-23 21:46:52 -0300

    refactor: split man-page constants

---
+++ src/generators/man-page/constants.mjs
@@ -0,0 +1,11 @@
+'use strict';
+
+// https://github.com/nodejs/node/blob/main/doc/api/cli.md#options
+// This slug should reference the section where the available
+// options are defined.
+export const DOC_SLUG_OPTIONS = 'options';
+
+// https://github.com/nodejs/node/blob/main/doc/api/cli.md#environment-variables-1
+// This slug should reference the section where the available
+// environment variables are defined.
+export const DOC_SLUG_ENVIRONMENT = 'environment-variables-1';
