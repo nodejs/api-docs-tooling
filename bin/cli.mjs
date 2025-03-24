5f7d88cf34547a44352bd42be482ea2892b826bc
Aviv Keller <me@aviv.sh>
2025-03-24 06:41:01 -0400

    fixup!

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { resolve } from 'node:path';
-import { argv, exit } from 'node:process';
+import { argv } from 'node:process';
 
 import { Command, Option } from 'commander';
 
