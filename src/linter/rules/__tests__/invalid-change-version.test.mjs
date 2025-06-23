492c2da548e0e5adc28254313e5bcb6bf7c7ca28
Guilherme Araújo <arauujogui@gmail.com>
2025-06-23 12:53:21 -0300

    feat: improve changes issues positions (#313)
    * feat: improve changes issues positions
    
    * fix: some fixes
    
    * refactor: some improvements
    
    * refactor: code review
    
    * test: create skippable test cases
    
    * fix: dedent alignment
    
    * test: remove wrong node type

--- src/linter/rules/__tests__/invalid-change-version.test.mjs
+++ src/linter/rules/__tests__/invalid-change-version.test.mjs
@@ -12,14 +12,14 @@ import { createContext } from './utils.mjs';
 describe('invalidChangeVersion', () => {
   it('should not report if all change versions are non-empty', () => {
     const yamlContent = dedent`
-<!-- YAML
-changes:
-  - version:
-      - v15.7.0
-      - v14.18.0
-  - version: v6.4.0
-  - version: v5.0.0
--->`;
+      <!-- YAML
+      changes:
+        - version:
+            - v15.7.0
+            - v14.18.0
+        - version: v6.4.0
+        - version: v5.0.0
+      -->`;
 
     const context = createContext([
       {
@@ -35,14 +35,11 @@ changes:
 
   it('should report an issue if a change version is missing', () => {
     const yamlContent = dedent`
-<!-- YAML
-changes:
-  - version:
-      - v15.7.0
-      - v14.18.0
-  - version: v6.4.0
-  - version:
--->`;
+      <!-- YAML
+      changes:
+        - version:
+        - pr-url: https://github.com/nodejs/node/pull/1
+      -->`;
 
     const context = createContext([
       {
@@ -57,17 +54,27 @@ changes:
 
     invalidChangeVersion(context);
 
-    strictEqual(context.report.mock.callCount(), 1);
+    strictEqual(context.report.mock.callCount(), 2);
 
-    const call = context.report.mock.calls[0];
+    const callArguments = context.report.mock.calls.flatMap(
+      call => call.arguments
+    );
 
-    deepStrictEqual(call.arguments, [
+    deepStrictEqual(callArguments, [
       {
         level: 'error',
         message: 'Missing version field in the API doc entry',
         position: {
-          start: { line: 1, column: 1, offset: 1 },
-          end: { line: 1, column: 1, offset: 1 },
+          start: { line: 3 },
+          end: { line: 3 },
+        },
+      },
+      {
+        level: 'error',
+        message: 'Missing version field in the API doc entry',
+        position: {
+          start: { line: 4 },
+          end: { line: 4 },
         },
       },
     ]);
@@ -104,14 +111,14 @@ changes:
 
   it('should not report if all change versions are valid', () => {
     const yamlContent = dedent`
-<!-- YAML
-changes:
-  - version:
-      - v15.7.0
-      - v14.18.0
-  - version: v6.4.0
-  - version: v5.0.0
--->`;
+      <!-- YAML
+      changes:
+        - version:
+            - v15.7.0
+            - v14.18.0
+        - version: v6.4.0
+        - version: v5.0.0
+      -->`;
 
     const context = createContext([
       {
@@ -127,14 +134,14 @@ changes:
 
   it('should report an issue if a change version is invalid', () => {
     const yamlContent = dedent`
-<!-- YAML
-changes:
-  - version:
-      - v13.9.0
-      - INVALID_VERSION
-  - version: v6.4.0
-  - version: v5.0.0
--->`;
+      <!-- YAML
+      changes:
+        - version:
+            - v13.9.0
+            - INVALID_VERSION
+        - version: v6.4.0
+        - version: v5.0.0
+      -->`;
 
     const context = createContext([
       {
@@ -155,8 +162,8 @@ changes:
         level: 'error',
         message: 'Invalid version number: INVALID_VERSION',
         position: {
-          start: { column: 1, line: 7, offset: 103 },
-          end: { column: 35, line: 7, offset: 137 },
+          start: { line: 11 },
+          end: { line: 11 },
         },
       },
     ]);
@@ -164,14 +171,14 @@ changes:
 
   it('should report an issue if a change version contains a REPLACEME and a version', () => {
     const yamlContent = dedent`
-<!-- YAML
-changes:
-  - version:
-      - v24.0.0
-      - REPLACEME
-  - version: v6.4.0
-  - version: v5.0.0
--->`;
+      <!-- YAML
+      changes:
+        - version:
+            - v24.0.0
+            - REPLACEME
+        - version: v6.4.0
+        - version: v5.0.0
+      -->`;
 
     const context = createContext([
       {
@@ -192,10 +199,150 @@ changes:
         level: 'error',
         message: 'Invalid version number: REPLACEME',
         position: {
-          start: { column: 1, line: 7, offset: 103 },
-          end: { column: 35, line: 7, offset: 137 },
+          start: { line: 11 },
+          end: { line: 11 },
         },
       },
     ]);
+  });
+
+  it('should report an issue if changes is not a sequence', () => {
+    const yamlContent = dedent`
+      <!-- YAML
+      changes:
+        abc:
+        def:
+      -->`;
+
+    const context = {
+      tree: {
+        type: 'root',
+        children: [
+          {
+            type: 'html',
+            value: yamlContent,
+            position: {
+              start: { column: 1, line: 7, offset: 103 },
+              end: { column: 35, line: 7, offset: 137 },
+            },
+          },
+        ],
+      },
+      report: mock.fn(),
+      getIssues: mock.fn(),
+    };
+
+    invalidChangeVersion(context);
+    strictEqual(context.report.mock.callCount(), 1);
+    const call = context.report.mock.calls[0];
+    deepStrictEqual(call.arguments, [
+      {
+        level: 'error',
+        message: 'Invalid change property type',
+        position: {
+          start: { line: 8 },
+          end: { line: 8 },
+        },
+      },
+    ]);
+  });
+
+  it('should report an issue if version is not a mapping', () => {
+    const yamlContent = dedent`
+      <!-- YAML
+      changes:
+        version:
+          - abc
+          - def
+      -->`;
+
+    const context = {
+      tree: {
+        type: 'root',
+        children: [
+          {
+            type: 'html',
+            value: yamlContent,
+            position: {
+              start: { column: 1, line: 7, offset: 103 },
+              end: { column: 35, line: 7, offset: 137 },
+            },
+          },
+        ],
+      },
+      report: mock.fn(),
+      getIssues: mock.fn(),
+    };
+
+    invalidChangeVersion(context);
+    strictEqual(context.report.mock.callCount(), 1);
+    const call = context.report.mock.calls[0];
+    deepStrictEqual(call.arguments, [
+      {
+        level: 'error',
+        message: 'Invalid change property type',
+        position: {
+          start: { line: 8 },
+          end: { line: 8 },
+        },
+      },
+    ]);
+  });
+
+  it("should skip validations if yaml root node isn't a mapping", () => {
+    const yamlContent = dedent`
+      <!-- YAML
+      - abc
+      - def
+      -->`;
+
+    const context = {
+      tree: {
+        type: 'root',
+        children: [
+          {
+            type: 'html',
+            value: yamlContent,
+            position: {
+              start: { column: 1, line: 7, offset: 103 },
+              end: { column: 35, line: 7, offset: 137 },
+            },
+          },
+        ],
+      },
+      report: mock.fn(),
+      getIssues: mock.fn(),
+    };
+
+    invalidChangeVersion(context);
+    strictEqual(context.report.mock.callCount(), 0);
+  });
+
+  it('should skip validations if changes node is missing', () => {
+    const yamlContent = dedent`
+      <!-- YAML
+      added: v0.1.91
+      -->`;
+
+    const context = {
+      tree: {
+        type: 'root',
+        children: [
+          {
+            type: 'html',
+            value: yamlContent,
+            position: {
+              start: { column: 1, line: 7, offset: 103 },
+              end: { column: 35, line: 7, offset: 137 },
+            },
+          },
+        ],
+      },
+      report: mock.fn(),
+      getIssues: mock.fn(),
+    };
+
+    invalidChangeVersion(context);
+    strictEqual(context.report.mock.callCount(), 0);
   });
 });
