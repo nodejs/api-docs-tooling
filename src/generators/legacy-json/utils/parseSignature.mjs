a0841621d7ce05025e04d1bd8e9739f7ef4a5920
Aviv Keller <me@aviv.sh>
2025-06-27 14:41:42 -0400

    Remove redudant `' '`

--- src/generators/legacy-json/utils/parseSignature.mjs
+++ src/generators/legacy-json/utils/parseSignature.mjs
@@ -2,7 +2,7 @@
 
 import { PARAM_EXPRESSION } from '../constants.mjs';
 
-const OPTIONAL_LEVEL_CHANGES = { '[': 1, ']': -1, ' ': 0 };
+const OPTIONAL_LEVEL_CHANGES = { '[': 1, ']': -1 };
 
 /**
  * @param {Number} depth
