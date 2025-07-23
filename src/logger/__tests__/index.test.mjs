5116d0035a6a9ab0f70a7cec40927c1d5dec824c
Guilherme Araújo <arauujogui@gmail.com>
2025-07-23 19:08:51 -0300

    test: fix logger init

--- src/logger/__tests__/index.test.mjs
+++ src/logger/__tests__/index.test.mjs
@@ -1,19 +1,15 @@
-import { ok, strictEqual } from 'node:assert';
+import { strictEqual } from 'node:assert';
 import { describe, it } from 'node:test';
 
 import { Logger } from '../index.mjs';
 
 describe('Logger singleton', () => {
   it('should return the same instance on multiple calls', () => {
+    Logger.init('console');
+
     const logger1 = Logger.getInstance();
     const logger2 = Logger.getInstance();
 
     strictEqual(logger1, logger2);
-  });
-
-  it('should create an instance of logger', () => {
-    const logger = Logger.getInstance();
-
-    ok(logger);
   });
 });
